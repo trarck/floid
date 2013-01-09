acaaac686bae2bfe19e8f825cf9d616fb9f0607c
Giuseppe Barba <giuseppe.barba@st.com>
2013-01-09 13:40:36 +0100

    Gralloc: Fix FPS formula.
    Fix returned FPS value cosidering vsync and hsync values
    in its calculation.
    
    Signed-off-by: Giuseppe Barba <giuseppe.barba@st.com>

--- SPEAr1340/hardware/libhardware/modules/gralloc/framebuffer_device.cpp
+++ SPEAr1340/hardware/libhardware/modules/gralloc/framebuffer_device.cpp
@@ -270,8 +270,8 @@ int init_frame_buffer_locked(struct private_module_t* module)
 	{
 		refreshRate = 1000000000000000LLU /
 		(
-			uint64_t( info.upper_margin + info.lower_margin + info.yres )
-			* ( info.left_margin  + info.right_margin + info.xres )
+			uint64_t( info.upper_margin + info.lower_margin + info.yres + info.vsync_len)
+			* ( info.left_margin  + info.right_margin + info.xres + info.hsync_len)
 			* info.pixclock
 		);
 	}
